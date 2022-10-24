be3af318410611fe410f8f2555092b0629876517
mohamedkonate <mohamed.konate@Live.fr>
2022-10-24 15:49:18 +0200

    exercice 1

--- EX1/src/main/java/Test.java
+++ EX1/src/main/java/Test.java
@@ -1,9 +1,13 @@
 public class Test {
     public static void main(String args[]) {
 
-        fonctionnelle<Integer> addition = (a, b) -> a + b;
+        fonctionnelle<Integer> addition_int = (a, b) -> a + b;
+        fonctionnelle<Double> addition_Double = (a, b) -> a + b;
+        fonctionnelle<String> addition_String = (a, b) -> a + b;
+        fonctionnelle<Long> addition_Long= (a, b) -> a + b;
 
-        System.out.println(" 11 + 3 = " + addition.somme(11, 3));
+
+        System.out.println(" 11 + 3 = " + addition_int.somme(11, 3));
 
     }
 
